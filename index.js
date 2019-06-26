3920af4df599f80598c46113e2de9eb0b799968d
Karthikeyan S <k@kaos.agency>
2019-06-26 19:14:02 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+exports.disciplines = [
+  'discipline.mathematics',
+  'discipline.physics',
+  'discipline.chemistry',
+  'discipline.dataProcessing',
+];
+
+exports.tutorLevels = [
+  'level.primary',
+  'level.middleSchol',
+  'level.2nd',
+  'level.1st',
+  'level.terminal',
+  'level.bts',
+  'level.licence',
+  'level.master',
+  'level.adultTraining',
+];
+
+exports.studentLevels = [
+  'level.cp',
+  'level.ce1',
+  'level.ce2',
+  'level.cm1',
+  'level.cm2',
+  'level.6th',
+  'level.5th',
+  'level.4th',
+  'level.3rd',
+  'level.2nd',
+  'level.1st',
+  'level.terminal',
+  'level.bts',
+  'level.licence',
+  'level.master',
+  'level.adultTraining',
+];
+
+exports.levelGroups = [
+  'levelGroup.primary',
+  'levelGroup.middleSchool',
+  'levelGroup.highSchool',
+  'levelGroup.superior',
+  'levelGroup.adultTraining',
+];
+
+exports.levelToGroupMap = {
+  'level.primary': 'levelGroup.primary',
+  'level.cp': 'levelGroup.primary',
+  'level.ce1': 'levelGroup.primary',
+  'level.ce2': 'levelGroup.primary',
+  'level.cm1': 'levelGroup.primary',
+  'level.cm2': 'levelGroup.primary',
+  'level.middleSchol': 'levelGroup.middleSchool',
+  'level.6th': 'levelGroup.middleSchool',
+  'level.5th': 'levelGroup.middleSchool',
+  'level.4th': 'levelGroup.middleSchool',
+  'level.3rd': 'levelGroup.middleSchool',
+  'level.2nd': 'levelGroup.highSchool',
+  'level.1st': 'levelGroup.highSchool',
+  'level.terminal': 'levelGroup.highSchool',
+  'level.bts': 'levelGroup.superior',
+  'level.licence': 'levelGroup.superior',
+  'level.master': 'levelGroup.superior',
+  'level.adultTraining': 'levelGroup.adultTraining',
+};
+
+exports.titles = [
+  'title.student',
+  'title.teacher',
+  'title.professor',
+  'title.doctor',
+];
+
+exports.salutations = [
+  'salutation.mr',
+  'salutation.mrs',
+  'salutation.ms',
+];
